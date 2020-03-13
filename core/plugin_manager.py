28cfe196de92e3ea573b664637db50164a61a462
Wesley <wescdem@gmail.com>
2020-03-13 15:20:21 -0400

    Made Plugin Manager Recursive
    The old plugin manager didn't make sure the dependencies of plugins had their own dependencies there. Implemented a recursive function to check.

--- core/plugin_manager.py
+++ core/plugin_manager.py
@@ -22,6 +22,24 @@ class PluginManager:
         return config
 
     @classmethod
+    def _check_dep_recur(cls, plufin_config, plugins):
+        """
+        This is a recursive function that makes sure every dependency has their own dependencies present
+        :param plufin_config: The config of the plugin you are checking
+        :param plugins: The list of plugins
+        :return: True or False statement
+        """
+        dep_fullfilled = 0
+        for t in plugins:
+            for plugin, p_config in plugins[t]:
+                if p_config["id"] in plufin_config["dependencies"]:
+                    if PluginManager._check_dep_recur(p_config, plugins):
+                        dep_fullfilled += 1
+                    else:
+                        print(f"{plufin_config['name']} will not be loaded. Missing dependency: {p_config['id']}")
+        return dep_fullfilled == len(plufin_config["dependencies"])
+
+    @classmethod
     def get_valid_plugins(cls, plugin_type: str, debug: bool = True) -> List[str]:
         """
         This gets all the plugins which have their dependencies fulfilled.
@@ -41,22 +59,7 @@ class PluginManager:
         for t in plugin_type.split("-"):
             if t in plugins:
                 for plugin, config in plugins[t]:
-                    load = [True]
-                    for d in config["dependencies"]:
-                        has_plugin = False
-                        for p_type in plugins:
-                            for p in plugins[p_type]:
-                                if p[1]["id"] == d:
-                                    has_plugin = True
-                                    break
-                            if has_plugin:
-                                break
-                        if not has_plugin:
-                            load = False
-                            if debug:
-                                print(f"{config['name']} will not be loaded. Missing dependency: {d}")
-                            break
-                    if load[0]:
+                    if PluginManager._check_dep_recur(config, plugins):
                         valid_plugins.append(config["id"])
 
         return valid_plugins
